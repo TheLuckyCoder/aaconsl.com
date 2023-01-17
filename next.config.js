acbd703b410797457a6307e4901da8a5c1c9dfc8
Razvan Filea <razvan.filea@gmail.com>
2023-01-17 20:56:45 +0200

    Remove Preact as it's causing some issues

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-
-    // Note, preact is only enabled for production builds (`next build`)
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat"
-      };
-    }
-
-    return config;
-  }
 }
